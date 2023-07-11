a3966edbab3c5b41d45af826da016accb7474689
Matheus Milanez <mattmila96@gmail.com>
2023-07-11 17:12:46 -0300

    requisito 9

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -7,8 +7,8 @@ export default interface Fighter extends SimpleFighter{
   defense: number,
   energy?: Energy
 
-  attack(enemy: Fighter): void,
-  special?(enemy: Fighter): void,
+  attack(enemy: SimpleFighter): void,
+  special?(enemy: SimpleFighter): void,
   levelUp(): void,
   receiveDamage(attackPoints: number): number,
 }
